17009e507949af0d1f8ef562ce38a06a532472be
Jhengsh <jhengsh.email@gmail.com>
2020-05-23 22:06:54 +0800

    update python version

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ CLASSIFIERS = [
     'Intended Audience :: System Administrators',
     'Natural Language :: English',
     'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
@@ -16,7 +19,7 @@ CLASSIFIERS = [
 
 setup(
     name='blob-cli',
-    version=open('VERSION').read().strip(),
+    version='0.0.2',
     author='Hsueh-Hung Cheng',
     author_email='jhengsh.email@gmail.com',
     url='https://github.com/Jhengsh/blob-cli',
@@ -31,6 +34,6 @@ setup(
     packages=find_packages(),
     license='MIT',
     platforms='any',
-    python_requires='!=2.*,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+    python_requires='!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
     install_requires=["azure-storage-blob"],
 )
